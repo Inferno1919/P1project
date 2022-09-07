51cdb6e47a7ee2666e2899bee05c50916593ace9
Jake Speitel <jakespeitel19@gmail.com>
2022-09-07 01:17:30 -0400

    makes the random image fetch into a function and adds the ability to select a specific breed

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+var breed = "african";
+
 //make long, reused links into variables
 const heartEmpty = ("https://toppng.com/uploads/preview/black-love-heart-outline-11563250728brya8dxczz.png")
 const heartFilled = ("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png")
 
-//fetches the random dog image
-fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json())
-    .then(function(parsed){
-        displayDog(parsed["message"]);
-    })
+//get initial image when page is loaded
+randDog(breed);
+
 
 //when like button is pressed it checks if the image has been liked yet and changes the image accordingly
 document.getElementById("likeButton").addEventListener("click", function(){
@@ -15,11 +14,29 @@ document.getElementById("likeButton").addEventListener("click", function(){
     if(likeButton.src == heartFilled){
         likeButton.src= heartEmpty;
     }else{
-        likeButton.src= heartFilled
+        likeButton.src= heartFilled;
     }
 })
 
 //displays main dog image
 function displayDog(image){
     document.getElementById("mainDog").src=image;
+}
+//fetch random dog image based on breed selection
+function randDog(breed){
+
+    if(breed == "Select Breed"){
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(function(parsed){
+            displayDog(parsed["message"]);
+        })
+    } else{
+        fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+            .then(response => response.json())
+            .then(function(parsed){
+            displayDog(parsed["message"]);
+        })
+    
+    }
 }
